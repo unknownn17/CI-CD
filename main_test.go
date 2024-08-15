f868f243a0ae80c91451fd91b0d470e6d5b3c31e
Your Name <dostonxoshimov2005@gmail.com>
2024-08-15 18:40:48 +0500

    ad

--- main_test.go
+++ main_test.go
@@ -3,20 +3,29 @@ package main
 import (
 	"encoding/json"
 	"net/http"
+	"net/http/httptest"
 	"testing"
 )
 
-func Test_api(t *testing.T){
-	want:="Hello"
-	res,err:=http.Get("http://localhost:8080/health")
-	if err!=nil{
-		t.Error(err)
+func Test_api(t *testing.T) {
+	want := "Hello"
+	
+	req, err := http.NewRequest("GET", "/health", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rr := httptest.NewRecorder()
+
+	Get(rr, req)
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusOK)
 	}
 	var have string
-	if err:=json.NewDecoder(res.Body).Decode(&have);err!=nil{
+	if err := json.NewDecoder(rr.Body).Decode(&have); err != nil {
 		t.Error(err)
 	}
-	if want!=have{
-		t.Error(err)
+	if want != have {
+		t.Errorf("handler returned unexpected body: got %v want %v", have, want)
 	}
 }
